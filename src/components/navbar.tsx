f0ca9effe2e21b1240f40806fca9891ab30de887
Himanish kochhar <himanish.kochhar@gmail.com>
2024-08-08 10:34:22 +0530

    Added the ipfs api secrets and logo added

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,7 +9,7 @@ export const Navbar = () => {
         <section className='navbar  '>
             <div className='flex flex-row justify-between items-center'>
                 <div className='flex flex-row justify-center items-center '>
-                    <img src="./SolanaLogoSf.png" alt="Logo" width="80px"
+                    <img src="./solanaMain.png" alt="Logo" width="80px"
                         height="80px" />
                     <h2 className='text-primary teko-regular font-bold scroll-m-20 text-4xl tracking-normal'>TokenLaunchpad</h2>
                 </div>

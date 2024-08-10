0fa63012c01abe681c5d6c64da33e50187592adf
Himanish kochhar <himanish.kochhar@gmail.com>
2024-08-10 11:25:02 +0530

    Added the Token Add functionality with file uploads

--- src/components/TokenLaunchpadForm.tsx
+++ src/components/TokenLaunchpadForm.tsx
@@ -19,6 +19,10 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { handleUpload } from "@/lib/fileupload"
+import { Keypair, SystemProgram, Transaction } from "@solana/web3.js"
+import { createInitializeMetadataPointerInstruction, createInitializeMintInstruction, ExtensionType, getMintLen, LENGTH_SIZE, TOKEN_2022_PROGRAM_ID, TYPE_SIZE } from "@solana/spl-token"
+import { createInitializeInstruction, pack } from '@solana/spl-token-metadata';
+import { useConnection, useWallet } from "@solana/wallet-adapter-react"
 
 const formSchema = z.object({
     name: z.string().min(2, {
@@ -27,7 +31,10 @@ const formSchema = z.object({
     symbol: z.string().min(1, {
         message: "Symbol should be at least 1 character"
     }),
-    imageURL: z
+    description: z.string().min(5, {
+        message: "Description should be at least 5 character"
+    }),
+    imageFile: z
         .instanceof(File)
         .refine((file) => file.type === "image/jpeg" || file.type === "image/png", {
             message: "File must be in .jpg, .jpeg, or .png format",
@@ -38,30 +45,91 @@ const formSchema = z.object({
 })
 
 export function TokenLaunchpad() {
+    const wallet = useWallet();
+    const { connection } = useConnection();
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
             name: '',
             symbol: '',
-            imageURL: undefined,
+            description: '',
+            imageFile: undefined,
             supply: 0
 
         },
         mode: 'onChange',
     });
-    const onSubmit: SubmitHandler<z.infer<typeof formSchema>> = (values: z.infer<typeof formSchema>) => {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
+    const onSubmit: SubmitHandler<z.infer<typeof formSchema>> = async (values: z.infer<typeof formSchema>) => {
+
+        const metaDataURI = await handleFileUpload({ ...values })
+        if (wallet.publicKey) {
+            await createToken({ ...values, metaDataURI, walletPubkey: wallet.publicKey })
+
+        }
+
     }
-    async function handleFileUpload(file: File) {
-        const response = await handleUpload(file)
-        console.log(response)
+
+    async function handleFileUpload({ name, symbol, description, imageFile }: z.infer<typeof formSchema>) {
+        const ImageUri = await handleUpload(imageFile)
+        const metaDataJson = {
+            name,
+            symbol,
+            description,
+            image: ImageUri
+        }
+
+        const jsonFile = new File([JSON.stringify(metaDataJson)], 'metadata.json', { type: 'application/json' });
+        const metaDataURI = await handleUpload(jsonFile)
+        return metaDataURI
+
+
+    }
+
+    async function createToken({ metaDataURI, name, symbol, walletPubkey }: any) {
+        const mintKeypair = Keypair.generate();
+        const metaData = {
+            mint: mintKeypair.publicKey,
+            name,
+            symbol,
+            uri: metaDataURI,
+            additionalMetadata: []
+        }
+        const mintLen = getMintLen([ExtensionType.MetadataPointer])
+        const metadataLen = TYPE_SIZE + LENGTH_SIZE + pack(metaData).length
+        const lamports = await connection.getMinimumBalanceForRentExemption(mintLen + metadataLen);
+
+        const transaction = new Transaction().add(
+            SystemProgram.createAccount({
+                fromPubkey: walletPubkey,
+                newAccountPubkey: mintKeypair.publicKey,
+                lamports,
+                space: mintLen,
+                programId: TOKEN_2022_PROGRAM_ID
+            }),
+            createInitializeMetadataPointerInstruction(mintKeypair.publicKey, walletPubkey, mintKeypair.publicKey, TOKEN_2022_PROGRAM_ID),
+            createInitializeMintInstruction(mintKeypair.publicKey, 9, walletPubkey, null, TOKEN_2022_PROGRAM_ID),
+            createInitializeInstruction({
+                programId: TOKEN_2022_PROGRAM_ID,
+                mint: mintKeypair.publicKey,
+                metadata: mintKeypair.publicKey,
+                name: metaData.name,
+                symbol: metaData.symbol,
+                uri: metaData.uri,
+                mintAuthority: walletPubkey,
+                updateAuthority: walletPubkey
+            }))
+        transaction.feePayer = walletPubkey
+        transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash
+        transaction.partialSign(mintKeypair)
+        await wallet.sendTransaction(transaction, connection)
+
+        console.log(`Token mint created at ${mintKeypair.publicKey.toBase58()}`)
+
     }
 
 
-    return (
-        <Card className="sm:w-[400px] md:w-[1000px] ">
+    return (<div>
+        {wallet.connected && <Card className="sm:w-[400px] md:w-[1000px] ">
             <CardHeader>
                 <CardTitle className="text-3xl md:text-center md:p-2">Solana Token Generate</CardTitle>
 
@@ -98,7 +166,20 @@ export function TokenLaunchpad() {
                                     )}
                                 />
                                 <FormField
-                                    name="imageURL"
+                                    name="description"
+                                    control={form.control}
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Description</FormLabel>
+                                            <FormControl>
+                                                <Input placeholder="description " {...field} />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    name="imageFile"
                                     control={form.control}
                                     render={({ field }) => (
                                         <FormItem>
@@ -108,7 +189,6 @@ export function TokenLaunchpad() {
                                                     const file = e.target.files?.[0]
                                                     if (file) {
                                                         field.onChange(file);
-                                                        handleFileUpload(file);
                                                     }
 
                                                 }} />
@@ -144,6 +224,8 @@ export function TokenLaunchpad() {
             </CardContent>
 
         </Card>
+        }
+    </div>
 
     )
 }

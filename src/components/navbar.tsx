e1095bfbd80e485d5c43e2a7e9793c4cd43608fd
Himanish kochhar <himanish.kochhar@gmail.com>
2024-09-07 09:34:12 +0530

    navbar edited with resposive layout

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,21 +1,21 @@
 
-import React from 'react'
 import {
     WalletMultiButton
 } from '@solana/wallet-adapter-react-ui'
 import { ModeToggle } from './mode-toggle'
 export const Navbar = () => {
     return (
-        <section className='navbar  '>
-            <div className='flex flex-row justify-between items-center'>
-                <div className='flex flex-row gap-3 justify-center items-center '>
-                    <img src="./solanaLogoMark.svg" alt="Logo" width="50px"
-                        height="50px" />
-                    <h2 className='text-primary teko-regular font-bold scroll-m-20 text-3xl tracking-normal'>TokenLaunchpad</h2>
+        <section className='navbar'>
+            <div className='flex mobile:flex-col mobile:max-sm:gap-4 md:flex-row mobile:items-start md:justify-between md:items-center mobile:max-sm:p-2'>
+                <div className='flex flex-row mobile:gap-1 md:gap-3 justify-center items-center '>
+                    <img src="./solanaLogoMark.svg" alt="Logo" className={`mobile:w-[40px] md:w-[50px]`} />
+                    <h2 className='text-primary teko-regular mobile:max-sm:text-[24px] font-bold scroll-m-20 text-3xl tracking-normal'>TokenLaunchpad</h2>
                 </div>
-                <div className='flex gap-4 flex-row justify-center items-center'>
+                <div className='flex gap-4 flex-row mobile:max-sm:w-full mobile:items-center mobile:justify-end  md:justify-center md:items-center'>
                     <WalletMultiButton />
-                    <ModeToggle />
+                    <div className='mobile:max-sm:hidden'>
+                        <ModeToggle />
+                    </div>
                 </div>
 
             </div>
